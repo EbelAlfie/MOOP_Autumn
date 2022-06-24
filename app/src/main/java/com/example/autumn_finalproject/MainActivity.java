7c146e6dbfd5eabd0a1182155267f37c9d93b94e
Immanuel Giovano <immanuel.giovano@gmail.com>
2022-06-24 16:43:38 +0700

    Add Get lat lon function

--- app/src/main/java/com/example/autumn_finalproject/MainActivity.java
+++ app/src/main/java/com/example/autumn_finalproject/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.autumn_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
         lokasi = (Button) findViewById(R.id.btn_location);
 
         dbHandler = new DBHandler(MainActivity.this);
+
+        try { //Request Permission if not permitted
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         try {
             dbHandler.addNewWeather(1, "Bandung", "Clear", "30*C", "50%", "1.5m/s South");
--- app/src/main/java/com/example/autumn_finalproject/chooseLocation.java
+++ app/src/main/java/com/example/autumn_finalproject/chooseLocation.java
@@ -14,7 +14,9 @@ public class chooseLocation extends AppCompatActivity {
     EditText inputLocation;
     Button chooseLoc, getLoc;
     TextView city, weather, temper, humid, wind;
+    TextView txt_lat, txt_lon;
     private DBHandler dbHandler;
+    GpsTracker gpsTracker;
 
     private String url1 = "http://api.openweathermap.org/data/2.5/weather?mode=xml&APPID=af9001dce8720bbe36288523ce329664";
 
@@ -33,12 +35,15 @@ public class chooseLocation extends AppCompatActivity {
         humid = (TextView) findViewById(R.id.txt_humid);
         wind = (TextView) findViewById(R.id.txt_wind);
 
+        txt_lat = (TextView) findViewById(R.id.txt_lat);
+        txt_lon = (TextView) findViewById(R.id.txt_lon);
+
         dbHandler = new DBHandler(chooseLocation.this);
 
-        chooseLoc.setOnClickListener(new View.OnClickListener() {
+        chooseLoc.setOnClickListener(new View.OnClickListener() { //Get Weather With City Name
             @Override
             public void onClick(View view) {
-                String temp = inputLocation.getTransitionName();
+                String temp = String.valueOf(inputLocation.getText());
 
 //                Intent i = new Intent(getApplicationContext(), MainActivity.class);
 //                dbHandler.updateWeather(1, "Jakarta", "Overcast", "20*C", "25%", "1m/s North");
@@ -46,9 +51,11 @@ public class chooseLocation extends AppCompatActivity {
             }
         });
 
-        getLoc.setOnClickListener(new View.OnClickListener() {
+        getLoc.setOnClickListener(new View.OnClickListener() { //Get Weather with current location
             @Override
             public void onClick(View view) {
+                    txt_lat.setText(getLocs(1));
+                    txt_lon.setText(getLocs(2));
 
 //                Intent i = new Intent(getApplicationContext(), MainActivity.class);
 //                dbHandler.updateWeather(1, "Bandung", "Clear", "30*C", "50%", "1.5m/s South");
@@ -56,4 +63,26 @@ public class chooseLocation extends AppCompatActivity {
             }
         });
     }
+
+    public String getLocs(int ID) { //ID  1 = lat, ID 2 = lon
+        String t_lat = "0";
+        String t_lon = "0";
+        gpsTracker = new GpsTracker(chooseLocation.this);
+        if (gpsTracker.canGetLocation()) {
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            t_lat = String.valueOf(latitude);
+            t_lon = String.valueOf(longitude);
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+        if(ID == 1){
+        return t_lat;
+        } else if(ID == 2) {
+            return t_lon;
+        }else{
+            return "0";
+        }
+    }
+
 }
